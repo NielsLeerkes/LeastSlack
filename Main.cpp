d470c545c85c73abe2824a47d192e3d1fde46285
Niels Leerkes <leerkes.niels@gmail.com>
2021-03-02 12:52:09 +0100

    duration en machine gescheiden

--- Main.cpp
+++ Main.cpp
@@ -5,26 +5,52 @@
  *      Author: na_le
  */
 
-
 #include <iostream>
 #include <fstream>
+#include <string>
+#include "JobShop.h"
+int main(int argc, char **argv)
+{
+	int x;
+	char name[256];
+	int counter;
+	std::cout << "Enter the file name:";
+	std::cin.getline(name, 256);
+	std::cout << name << std::endl;
 
-int main(int argc, char **argv) {
-    int sum = 0;
-    int x;
-    char name[256];
-    std::cout << "Enter the file name:";
-    std::cin.getline(name, 256);
-    std::cout << name << std::endl;
-    std::ifstream istrm;
-    istrm.open(name);
+	std::ifstream istrm;
+	istrm.open(name);
 
-    while (istrm >> x)
-    {
-    std::cout << x << std::endl;
-    }
+	JobShop shopA();
+	int eolCounter =0;
+	while (istrm >> x)
+	{
+		if (x == '\0')
+							{
+								std::cout << std::endl;
+								++eolCounter;
+							}
+		if (eolCounter != 0)
+		{
+			if (counter == 0)
+			{
+				std::cout << "Machine: ";
+				++counter;
+			}
+			if (counter == 1)
+			{
+				std::cout << "Duration: ";
+				counter = 0;
+			}
 
-    istrm.close();
-    std::cout << "Sum = " << sum << std::endl;
-    return 0;
+
+			std::cout << x << "\t";
+		}
+
+
+	}
+
+	istrm.close();
+
+	return 0;
 }
