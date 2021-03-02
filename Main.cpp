00f93825083e893bcbd24380177c4f1aa164b0b8
Niels Leerkes <leerkes.niels@gmail.com>
2021-03-02 12:28:34 +0100

    Main weer terug naar een goede verssie

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,25 @@
  */
 
 
-
+#include <iostream>
+#include <fstream>
 
 int main(int argc, char **argv) {
+    int sum = 0;
+    int x;
+    char name[256];
+    std::cout << "Enter the file name:";
+    std::cin.getline(name, 256);
+    std::cout << name << std::endl;
+    std::ifstream istrm;
+    istrm.open(name);
 
+    while (istrm >> x)
+    {
+    std::cout << x << std::endl;
+    }
+
+    istrm.close();
+    std::cout << "Sum = " << sum << std::endl;
+    return 0;
 }
